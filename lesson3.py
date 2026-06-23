789210fc76d72377564beaa476dd8ab66f0e1228
Aleksandr Safonov <alex.safonov@yahoo.com>
2026-06-23 18:36:53 +0300

    Решение урока 3 (добавлено условие)

--- lesson3.py
+++ lesson3.py
@@ -1,3 +1,11 @@
+# Клик по кнопке с css-классом
+
+# Напишите скрипт. Шаги: 
+# 1. Открыть страницу http://uitestingplayground.com/classattr
+# 2. Кликнуть на синюю(!) кнопку
+# 3. Запустите скрипт 3 раза. Убедитесь, что код не требуется редактировать – скрипт всегда работает. 
+
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
